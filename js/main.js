aeb63f0dd1eef0beb8d4330ca512a82fe1e010d1
Kevin Cui JY <kevincui1230@gmail.com>
2019-09-19 18:20:55 -0500

    change details in index

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("20 November 2019 0:00:00");			
+		var endTime = new Date("30 November 2019 9:00:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
