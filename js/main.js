7ea4b8351bb23efdf20f3d17961a7045095a8270
George Shao <GeorgeShao123@gmail.com>
2020-06-15 15:52:22 -0400

    Updated application page

--- js/main.js
+++ js/main.js
@@ -295,7 +295,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("20 June 2020 12:00");			
+		var endTime = new Date("27 June 2020 24:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
